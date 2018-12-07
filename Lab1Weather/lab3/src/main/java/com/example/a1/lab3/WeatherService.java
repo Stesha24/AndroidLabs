85e4529aed168f3b5a6a337bb5adf0c2a53314ca
Anastasia Kvashnina <anastasia.kv97@gmail.com>
2018-12-07 22:18:39 +0600

    lab4. Add push notifications

--- Lab1Weather/lab3/src/main/java/com/example/a1/lab3/WeatherService.java
+++ Lab1Weather/lab3/src/main/java/com/example/a1/lab3/WeatherService.java
@@ -1,8 +1,14 @@
 package com.example.a1.lab3;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
@@ -38,11 +44,14 @@ public class WeatherService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+    private NotificationManager nm;
+    private final int NOTIFICATION_ID = 127;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         final int timer = intent.getIntExtra("timer", 0);
         final String city = intent.getStringExtra("city");
+
         System.out.println(city);
         if(!city.isEmpty()) {
             Toast.makeText(getApplicationContext(), "" + timer + city, Toast.LENGTH_SHORT).show();
@@ -68,6 +77,7 @@ public class WeatherService extends Service {
 
 
     public void sendReq (final String city) {
+        nm = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=a3af4d3264bc6e28866eb9e57e1bb87e";
         JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -81,6 +91,26 @@ public class WeatherService extends Service {
                     String degrees = main.getString("temp") + "°C";
                     WeatherToday weatherToday = new WeatherToday(cityName, degrees);
                     weatherToday.save();
+
+                    Intent intent1 = new Intent(getApplicationContext(), SettingsActivity.class);
+                    Notification.Builder builder = new Notification.Builder(getApplicationContext());
+                    PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent1, PendingIntent.FLAG_CANCEL_CURRENT);
+                    builder
+                            .setContentIntent(pendingIntent)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.mipmap.ic_launcher))
+                            .setTicker("new notification")
+                            .setWhen(System.currentTimeMillis())
+                            .setAutoCancel(true)
+                            .setContentTitle("Weather right now")
+                            .setContentText(cityName + ": " + degrees);
+
+                    Notification notification = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                        notification = builder.build();
+                    }
+                    nm.notify(NOTIFICATION_ID, notification);
+
                     Toast.makeText(getApplicationContext(), "New records was added!", Toast.LENGTH_SHORT).show();
                 } catch (JSONException e) {
                     System.out.println("I am in catch in sendReq");
